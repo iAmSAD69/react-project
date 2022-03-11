7146545f3316c89ee62538e6c02ae2d3751264f0
iAmSAD <asad.shabbir@techlogix.com>
2022-03-11 15:23:16 +0500

    init

---
+++ src/components/Speakers.js
@@ -0,0 +1,15 @@
+import {data} from "../../SpeakerData";
+import SpeakersList from "./SpeakersList";
+import Header from "./Header";
+import SpeakersToolbar from "./SpeakersToolbar";
+function Speakers()
+{
+    return(
+        <div className="container-fluid">
+            <Header/>
+            <SpeakersToolbar/>
+            <SpeakersList data={data}/>
+        </div>
+        )
+}
+export default Speakers;
